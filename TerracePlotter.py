524305564f5faf9627cf65b21c250eaa8b22931f
Fiona Clubb <f.clubb@ed.ac.uk>
2017-11-06 16:45:02 -0600

    Adding some of Andy's dip and dip direction code into the terrace plotter, not finished yet

--- TerracePlotter.py
+++ TerracePlotter.py
@@ -14,6 +14,11 @@ from LSDPlottingTools import colours
 import matplotlib.cm as cm
 from matplotlib import rcParams
 
+#---------------------------------------------------------------------------------------------#
+# CSV READERS
+# Functions to read in the csv files
+#---------------------------------------------------------------------------------------------#
+
 def read_terrace_csv(DataDirectory,fname_prefix):
     """
     This function reads in the csv file with the extension "_terrace_info.csv"
@@ -56,6 +61,11 @@ def read_channel_csv(DataDirectory,fname_prefix):
 
     return df
 
+#---------------------------------------------------------------------------------------------#
+# ANALYSIS FUNCTIONS
+# Functions to analyse the terrace info
+#---------------------------------------------------------------------------------------------#
+
 def filter_terraces(terrace_df,min_size=5000, max_size=1000000):
     """
     This function takes the initial terrace dataframe and sorts it to remove terraces
@@ -82,6 +92,104 @@ def filter_terraces(terrace_df,min_size=5000, max_size=1000000):
             terrace_df = terrace_df[terrace_df['TerraceID'] != i]
 
     return terrace_df
+
+def get_terrace_strike_and_dips(terrace_df, min_size=5000):
+    """
+    This function takes the initial terrace dataframe and calculates the dip and
+    strike of the terrace surfaces. Fits a polynomial surface to the distribution
+    of terrace elevations and then gets the strike and dip of this surface.
+
+    Args:
+        terrace_df: pandas dataframe with the terrace info
+        min_size (int): the minimum number of pixels for a terrace. Any smaller ones will be excluded.
+
+    Returns:
+        dataframe with filtered terrace info.
+
+    Author: AW, ported by FJC
+    """
+    from scipy import linalg
+
+    # filter the terraces to remove ones that are too small
+    filter_terraces(terrace_df,min_size)
+    terraceIDs = list(terrace_df.TerraceID.values)
+
+    # get the info for each terrace ID
+    for terraceID in terraceIDs:
+        _x = terrace_df['DistAlongBaseline'].values
+        _y = terrace_df['DistToBaseline'].values
+        _z = terrace_df['Elevation'].values
+        _X = terrace_df['X'].values
+        _Y = terrace_df['Y'].values
+
+        # fit a plane to these points
+        # form: Z = C[0]*X + C[1]*Y + C[2]
+        _XY = np.vstack((_X, _Y, np.ones(len(_Y)))).transpose()
+        C,_,_,_ = linalg.lstsq(_XY, _z)
+        # Get dip and dip direction
+        _dip_slope = (C[0]**2 + C[1]**2)**.5
+        _dip = np.arctan(_dip_slope)
+
+        # Dip orientation is +/- this (pos/neg, not approx.)
+        print C[0], _dip
+        _dip_orientation_pm = np.arccos(C[0] / _dip_slope)
+        # Test which solution (+/-) is required
+        _isplus = (np.round(_dip * np.cos(_dip_orientation_pm),6) == np.round(C[0],6))
+        #print _dip * np.cos(_dip_orientation_pm), C[0]
+        # + C[1] * np.sin(_dip_orientation_pm)
+        _sign = 2 * _isplus - 1
+        _dip_orientation = _sign * _dip_orientation_pm
+        # Test which direction, now that we have orientation
+        # Which goes downwards?
+        _slope = C[0] * np.cos(_dip_orientation) + C[1] * np.sin(_dip_orientation)
+        #_neg = C[0] * np.cos(-_dip_orientation) + C[1] * np.sin(-_dip_orientation)
+        _ispos = _slope > 0#np.abs(_pos) < 0 #> np.abs(_neg)
+        # Flip with np.pi if the slope is going up -- dips go downslope
+        _dip_direction = _dip_orientation + np.pi * _ispos
+        # * (_ispos == 0)
+        # Test for dip direction -- this is like a difference, except first
+        # point is the origin
+        #_dip_direction_sign = np.sign( C[0] * np.cos(_dip_orientation) + C[1] * np.sin(_dip_orientation) )
+        #_dip_direction = _dip_direction_sign * _dip_orientation
+        #_dip_direction = (np.pi * (_dip_orientation < 0)) + _dip_orientation
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        #ax.scatter(_X, _Y, _z, c='blue', depthshade=True)
+        _x0, _y0, _z0 = np.mean(_X), np.mean(_Y), np.mean(_z)
+        _u0, _v0, _w0 = np.cos(_dip_direction), np.sin(_dip_direction), -_dip_slope
+        #_u0, _v0, _w0 = -np.cos(_dip_direction), -np.sin(_dip_direction), -_dip
+        #_u0, _v0, _w0 = C[0]/-_dip, C[1]/-_dip, -_dip
+        ax.quiver(_x0, _y0, _z0, _u0, _v0, _w0, length=50, arrow_length_ratio=.05, edgecolor='black', linewidth=4)
+        surf_xy = np.meshgrid( np.linspace(np.min(_X), np.max(_X), 20),
+        np.linspace(np.min(_Y), np.max(_Y), 20))
+        surf_z = C[0] * surf_xy[0] + C[1] * surf_xy[1] + C[2]
+        ax.plot_wireframe(surf_xy[0], surf_xy[1], surf_z, facecolors='k', alpha=1)
+        plt.title(terraceID)
+
+
+
+def get_terrace_areas(terrace_df, min_size=5000):
+    """
+    This function takes the initial terrace dataframe and calculates the
+    area of each terrace.
+
+    Args:
+        terrace_df: pandas dataframe with the terrace info
+
+
+    Returns:
+        dict where key is the terrace ID and value is the terrace area in m^2
+
+    Author: FJC
+    """
+    # filter the terraces to remove ones that are too small
+
+
+#---------------------------------------------------------------------------------------------#
+# XZ PLOTS
+# Functions to make XZ plots of terraces
+#---------------------------------------------------------------------------------------------#
 
 def long_profiler(DataDirectory,fname_prefix, min_size=5000, FigFormat='png', size_format='ESURF'):
     """
@@ -176,10 +284,10 @@ def long_profiler(DataDirectory,fname_prefix, min_size=5000, FigFormat='png', si
     ax.set_ylabel('Elevation (m)')
     plt.savefig(DataDirectory+fname_prefix+'_terrace_plot.'+FigFormat,format=FigFormat,dpi=300)
 
-    x_terraces = sorted(list(set(list(lp.DistAlongBaseline))))
-    z_terraces = []
-    for x_i in x_terraces:
-        z_terraces.append(np.mean(lp.Elevation.values[x_terraces == x_i]))
+#---------------------------------------------------------------------------------------------#
+# RASTER PLOTS
+# Functions to make raster plots of terrace attributes
+#---------------------------------------------------------------------------------------------#
 
 def MakeRasterPlotTerraceIDs(DataDirectory,fname_prefix, FigFormat='png', size_format='ESURF'):
     """
